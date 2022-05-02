658cc046d2b32818d847483acd87d4552a22cbf5
lawrimon <81292176+lawrimon@users.noreply.github.com>
2022-05-02 13:36:15 +0200

    Initial commit

---
+++ RouteNavigator/RouteNavigator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouteNavigator
+//
+//  Created by Laurin Tarta on 02.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worlds!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RouteNavigator/RouteNavigator/RouteNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteNavigatorApp.swift
+//  RouteNavigator
+//
+//  Created by Laurin Tarta on 02.05.22.
+//
+
+import SwiftUI
+
+@main
+struct RouteNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
