ad76206a7ae41ea7d357e596958e9da023e1b63f
Max Kiefer <inf20111@lehre.dhwb-stuttgart.de>
2022-05-27 15:03:47 +0200

    final unit tests added

--- RouteNavigator/RouteNavigatorTests/RouteNavigatorTests.swift
+++ RouteNavigator/RouteNavigatorTests/RouteNavigatorTests.swift
@@ -6,24 +6,12 @@
 //
 
 import XCTest
+import Theo
 @testable import RouteNavigator
 
 class RouteNavigatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testGetRoute() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
         let viewModel: NavigationViewModel = NavigationViewModel()
         let model: NavigationModel = NavigationModel()
         var points: [NavigationPoint] = []
@@ -32,13 +20,44 @@ class RouteNavigatorTests: XCTestCase {
         }
         for i in 0...806 {
             let routeWeight = model.getRoute(start: points[i], target: points[i+1]).1
-            print("testGetRoute \(i): Route from \(points[i].id) to \(points[i+1].id) with weight \(routeWeight!)")
+            print("testGetRoute [\(i)]: Route from \(points[i].id) to \(points[i+1].id) with weight \(routeWeight!)")
             XCTAssertNotNil(routeWeight)
         }
         let routeWeight = model.getRoute(start: points[807], target: points[0]).1
-        print("testGetRoute 807: Route from \(points[807].id) to \(points[0].id) with weight \(routeWeight!)")
+        print("testGetRoute [807]: Route from \(points[807].id) to \(points[0].id) with weight \(routeWeight!)")
         XCTAssertNotNil(routeWeight)
-        
+    }
+    
+    func testInitNavigationPoints() throws {
+        let model: NavigationModel = NavigationModel()
+        var points: [NavigationPoint] = []
+        points = model.initNavigationPoints()!
+        XCTAssertEqual(points.count, 808)
+    }
+    
+    func testNeo4jConnection() throws {
+        var test = false
+        var client: BoltClient?
+        do {
+        client = try BoltClient(hostname: "3d7caec0.databases.neo4j.io",
+                                    port: 7687,
+                                    username: "neo4j",
+                                    password: "nKtO5Z3vgY2n4Ilw5Bdqb8sPnAOBvqyNaUXAWMbk5n8",
+                                    encryption: Encryption.certificateIsSelfSigned)
+        } catch {
+            print("testNeo4jConnection: Failed during connection configuration")
+        }
+        guard let testClient = client else {return}
+        let result = testClient.connectSync()
+        switch result {
+        case .failure(_):
+            print("testNeo4jConnection: Error while connecting")
+            test = false
+        case .success(_):
+            print("testNeo4jConnection: Success while connecting")
+            test = true
+        }
+        XCTAssertTrue(test)
     }
 
 }
